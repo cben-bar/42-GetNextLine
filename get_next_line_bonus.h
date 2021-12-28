49aec82e0a9c39425443b678e34b851ea1062838
Clementine Ben barek <cben-bar@z4r11p5.42lyon.fr>
2021-12-28 01:25:19 +0100

    -

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: cben-bar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 01:49:49 by cben-bar          #+#    #+#             */
-/*   Updated: 2021/12/27 00:51:46 by cben-bar         ###   ########lyon.fr   */
+/*   Updated: 2021/12/28 00:58:10 by cben-bar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
-char	*ft_strchr(const char *str, int to_find);
+char	*ft_strchr(const char *str, int x);
 char	*ft_strndup(const char *src, int n);
 char	*ft_strdup(char const *src);
 char	*ft_strjoin(const char *str1, const char *str2);
